99af60eef2c02e0628cd0d22bf837ab3fde369ab
Brian Carlsen <carlsen.bri@gmail.com>
2023-03-09 20:11:30 +0100

    Checkpoint.

--- desktop/build.sh
+++ desktop/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Builds a release of the project.
 # Copies the binaries to a `thot-desktop-releases` folder,
-# Renaming the files as `<arch>-<vendor>-<system>-<subsystem>--<major_version>_<minor_version>_<patch_version>.<ext>`.
+# Renaming the files as `<arch>-<vendor>-<system>-<subsystem>--<major_version>_<minor_version>_<patch_version><ext>`.
 #
 # Accepts a relative path to the folder containing the signing files as the first argument.
 

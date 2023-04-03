2a929310a3260f2e979057d62328e1bbbbd01cfd
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2023-04-03 23:23:41 +0200

    added autosave to asset editor

--- desktop/src/components/details_bar/asset_editor.rs
+++ desktop/src/components/details_bar/asset_editor.rs
@@ -3,8 +3,8 @@ use crate::app::{AppStateAction, AppStateReducer};
 use crate::commands::common::UpdatePropertiesArgs;
 use crate::common::invoke;
 use crate::components::canvas::{GraphStateAction, GraphStateReducer};
-use crate::constants::MESSAGE_TIMEOUT;
-use thot_core::project::Asset as CoreAsset;
+use crate::hooks::use_asset;
+use thot_core::project::StandardProperties;
 use thot_core::types::ResourceId;
 use thot_ui::types::Message;
 use thot_ui::widgets::asset::AssetEditor as AssetEditorUi;
@@ -21,28 +21,19 @@ pub fn asset_editor(props: &AssetEditorProps) -> Html {
     let app_state = use_context::<AppStateReducer>().expect("`AppStateReducer` context not found");
     let graph_state =
         use_context::<GraphStateReducer>().expect("`GraphStateReducer` context not found");
+    let asset = use_asset(&props.rid);
 
-    let container = graph_state
-        .asset_map
-        .get(&props.rid)
-        .expect("`Asset`'s `Container` not found");
-
-    let container = graph_state
-        .graph
-        .get(container)
-        .expect("`Container` not found");
-
-    let asset = container.assets.get(&props.rid).expect("`Asset` not found");
-    let onsave = {
+    {
+        // Save changes on change
         let app_state = app_state.clone();
         let graph_state = graph_state.clone();
+        let asset = asset.clone();
 
-        Callback::from(move |asset: CoreAsset| {
-            let app_state = app_state.clone();
-            let graph_state = graph_state.clone();
-
-            spawn_local(async move {
-                let Ok(_) = invoke::<()>(
+        use_effect_with_deps(
+            move |asset| {
+                let asset = asset.clone();
+                spawn_local(async move {
+                    let Ok(_) = invoke::<()>(
                             "update_asset_properties",
                             &UpdatePropertiesArgs {
                                 rid: asset.rid.clone(),
@@ -57,18 +48,48 @@ pub fn asset_editor(props: &AssetEditorProps) -> Html {
                             return;
                         };
 
-                graph_state.dispatch(GraphStateAction::UpdateAsset(asset));
-                app_state.dispatch(AppStateAction::AddMessageWithTimeout(
-                    Message::success("Resource saved"),
-                    MESSAGE_TIMEOUT,
-                    app_state.clone(),
-                ));
+                    graph_state.dispatch(GraphStateAction::UpdateAsset((*asset).clone()));
+                });
+            },
+            asset,
+        );
+    }
+
+    let onchange_properties = {
+        let asset = asset.clone();
+        let app_state = app_state.clone();
+        let graph_state = graph_state.clone();
+
+        Callback::from(move |properties: StandardProperties| {
+            let asset = asset.clone();
+            let app_state = app_state.clone();
+            let graph_state = graph_state.clone();
+            spawn_local(async move {
+                let Ok(_) = invoke::<()>(
+                            "update_asset_properties",
+                            &UpdatePropertiesArgs {
+                                rid: asset.rid.clone(),
+                                properties: properties.clone(),
+                            },
+                        )
+                        .await else {
+                            app_state.dispatch(AppStateAction::AddMessage(Message::success(
+                                "Could not save resource",
+                            )));
+
+                            return;
+                        };
+
+                graph_state.dispatch(GraphStateAction::UpdateAsset((*asset).clone()));
+                let mut update = (*asset).clone();
+                update.properties = properties;
+                asset.set(update);
             });
         })
     };
 
     html! {
-        <AssetEditorUi asset={asset.clone()} {onsave} />
+        <AssetEditorUi asset={(*asset).clone()} {onchange_properties} />
     }
 }
 

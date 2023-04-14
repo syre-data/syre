68188a5da8aadf7c3a6680f10f98d921b8597381
Brian Carlsen <carlsen.bri@gmail.com>
2023-04-14 11:00:28 +0200

    Cleaned up exports from `core/db`.

--- core/src/db/mod.rs
+++ core/src/db/mod.rs
@@ -1,14 +1,8 @@
 //! Module for loading a Thot project.
-// pub mod collection;
-// pub mod database;
-// pub mod error;
 pub mod resource;
 pub mod search_filter;
 
 // Re-exports
-// pub use collection::Collection;
-// pub use database::Database;
-// pub use error::Error;
 pub use resource::{Resource, StandardResource};
 pub use search_filter::{SearchFilter, StandardSearchFilter};
 

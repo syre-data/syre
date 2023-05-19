f1e069ff9450f8da8e64ee474d73aad71e4f89fd
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2023-05-19 10:43:16 +0200

    Added logger that saves to file for src-tauri main

--- desktop/src-tauri/src/common.rs
+++ desktop/src-tauri/src/common.rs
@@ -4,6 +4,36 @@ use std::path::PathBuf;
 use thot_core::types::ResourceId;
 use thot_local::system::common;
 
+use crate::identifier::Identifier;
+use directories::ProjectDirs;
+use settings_manager::{Error as SettingsError, Result as SettingsResult};
+use std::io;
+use thot_core::identifier::Identifier as CoreIdentifier;
+
+/// Returns directories for the user's Thot.
+pub fn system_dirs() -> SettingsResult<ProjectDirs> {
+    let dirs_opt = ProjectDirs::from(
+        &CoreIdentifier::qualifier(),
+        &CoreIdentifier::organization(),
+        &Identifier::application(),
+    );
+
+    match dirs_opt {
+        Some(dirs) => Ok(dirs),
+        None => Err(SettingsError::IoError(io::Error::new(
+            io::ErrorKind::NotFound,
+            "system settings directory not found",
+        ))),
+    }
+}
+
+/// Returns the path to the user's config directory for Thot.
+pub fn config_dir_path() -> SettingsResult<PathBuf> {
+    let dirs = system_dirs()?;
+    let path = dirs.config_dir();
+    Ok(path.to_path_buf())
+}
+
 /// Path to user config directory.
 pub fn users_config_dir() -> Result<PathBuf> {
     let mut path = common::config_dir_path()?;
---
+++ desktop/src-tauri/src/identifier.rs
@@ -0,0 +1,8 @@
+/// Identifier information for Thot related to storing app data.
+pub struct Identifier;
+
+impl Identifier {
+    pub fn application() -> String {
+        String::from("desktop")
+    }
+}

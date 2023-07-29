21ef95f803e150859117e924684b7aea370aeabb
Brian Carlsen <carlsen.bri@gmail.com>
2023-07-29 23:42:34 +0200

    Trim whitespace from metadata keys and string values.

--- ui/src/widgets/metadata/metadatum_value_editor.rs
+++ ui/src/widgets/metadata/metadatum_value_editor.rs
@@ -234,6 +234,10 @@ fn value_from_input(value_ref: NodeRef, kind: &MetadatumType) -> JsResult<JsValu
     Ok(value)
 }
 
+/// Converts a value from one JSON value to another.
+///
+/// # Note
+/// + Strings are trimmed.
 fn convert_value(value: JsValue, target: &MetadatumType) -> JsValue {
     match (value.clone(), target.clone()) {
         (JsValue::String(_), MetadatumType::String)
@@ -247,7 +251,7 @@ fn convert_value(value: JsValue, target: &MetadatumType) -> JsValue {
             value.into()
         }
 
-        (JsValue::Number(value), MetadatumType::String) => value.to_string().into(),
+        (JsValue::Number(value), MetadatumType::String) => value.to_string().trim().into(),
 
         (JsValue::Array(value), MetadatumType::String) => serde_json::to_string_pretty(&value)
             .unwrap_or(String::default())

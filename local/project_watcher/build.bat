33520771a5cc16d63b1f0eacaadda135550bbfd0
Brian Carlsen <carlsen.bri@gmail.com>
2025-01-29 05:41:41 +0100

    Updated project-watcher build scripts for new crate name.

--- local/project_watcher/build.bat
+++ local/project_watcher/build.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
-set root=..\..
-set crate=\local\database
-set release_dir=%root%\target\release
+set PROGRAM_BASENAME="syre-project-watcher"
+set ROOT=..\..
+set release_dir=%ROOT%\target\release
 set crate_release_dir=target\release
-set lang=%root%\lang
+set lang=%ROOT%\lang
 
 :: get target
 for /f "tokens=* usebackq" %%o in (`rustc -Vv`) do (
@@ -16,15 +16,20 @@ for /f "tokens=1,2 delims= " %%a in ("%host%") do (
   set target=%%b
 )
 
-set target_out=%release_dir%\syre-local-database-%target%.exe
-set crate_target_out=%crate_release_dir%\syre-local-database-%target%.exe
+set target_out=%release_dir%\%PROGRAM_BASENAME%-%target%.exe
+set crate_target_out=%crate_release_dir%\%PROGRAM_BASENAME%-%target%.exe
 
 :: build
-if not exist "%release_dir%" mkdir "%release_dir%"
+if not exist "%release_dir%" md "%release_dir%"
 cargo build --release -F server
-move %crate_release_dir%\syre-local-database.exe %crate_target_out%
+move %crate_release_dir%\%PROGRAM_BASENAME%.exe %crate_target_out%
 
 :: copy to other directories
+set python_path=%lang%\python\src\syre\bin\
+set r_path=%lang%\r\inst\
+if not exist "%python_path%" md "%python_path%"
+if not exist "%r_path%" md "%r_path%"
+
 copy %crate_target_out% %target_out%
-copy "%target_out%" "%lang%\python\src\syre\bin\"
-copy "%target_out%" "%lang%\r\inst\"
+copy "%target_out%" "%python_path%"
+copy "%target_out%" "%r_path%"
--- local/project_watcher/build.sh
+++ local/project_watcher/build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+program_basenaem=syre-local-database
 root=../..
 releases=${root}/target/release
+crate_release_dir=target/release
 target=$(rustc -Vv | grep host | cut -f2 -d' ')
-target_file=syre-local-database-${target}
+target_file=${program_basenaem}-${target}
 target_out=${releases}/${target_file}
 
-cargo build --release -F server
 mkdir -p ${releases}
-# mv ${releases}/syre-local-database ${target_out}
-mv target/release/syre-local-database ${target_out}
+cargo build --release -F server
+mv ${crate_release_dir}/${program_basenaem} ${target_out}
 
 # copy to other packages
 lang=${root}/lang
 python_path=${lang}/python/src/syre/bin
 r_path=${lang}/r/inst
-
-# create python bin path if it does not exist
 mkdir -p ${python_path}
+mkdir -p ${r_path}
 
 cp ${target_out} ${python_path}
 cp ${target_out} ${r_path}

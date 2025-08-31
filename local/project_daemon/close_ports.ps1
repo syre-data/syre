b2be4134c55af192cebf78d29129683268113100
Brian Carlsen <carlsen.bri@gmail.com>
2025-08-31 19:47:53 +0200

    Version bumps and small updates.

--- local/project_daemon/close_ports.ps1
+++ local/project_daemon/close_ports.ps1
@@ -4,4 +4,4 @@ $SYRE_PUB_PORT = 7048
 
 $OUT = Get-Process -Id (Get-NetTCPConnection -LocalPort $SYRE_PUB_PORT).OwningProcess # Handles, NPM(K), PM(K), WS(K), CPU(s), Id, SI, ProcessName
 $PIDS = $OUT | Select-Object -Property Id, ProcessName
-$PIDS | ForEach-Object -Process {if ($_.ProcessName -eq $SYRE_DB_PROCESS_NAME) {TASKKILL /F /PID $_.Id }}
+$PIDS | ForEach-Object -Process {if ($_.ProcessName -eq $SYRE_DB_PROCESS_NAME) {TASKKILL /F /PID $_.Id}}
